c389f284f6e81b13919061eb21c50f6f31c9796c
liem nguyen <elminguyen@gmail.com>
2024-09-16 18:39:00 +0300

    api gateway with node

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import {ClientRequest} from 'http';
 
 import {notFound, errorHandler} from './middlewares';
-import api from './api';
 import {MessageResponse} from './types/Messages';
 
 const app = express();
@@ -14,15 +15,55 @@ const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+app.disable('x-powered-by');
 app.use(express.json());
+
+// Routes and microservices
+const services = [
+  {
+    route: '/api1',
+    target: 'https://pokeapi.co/api/v2/pokemon',
+  },
+  {
+    route: '/api2',
+    target: 'https://jsonplaceholder.typicode.com/users',
+  },
+  {
+    route: '/weather',
+    target: 'https://api.openweathermap.org/data/2.5/weather',
+     // Add additional proxy options for this service
+     on: {
+       proxyReq: (proxyReq: ClientRequest) => {
+       // Append apikey query parameter to the target URL
+       proxyReq.path += '&appid=' + process.env.API_KEY;
+       console.log(proxyReq);
+       console.log(process.env.API_KEY);
+      },
+    },
+  },
+];
+
+services.forEach(({ route, target, on }) => {
+  const proxyOptions = {
+    on,
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^${route}`]: '',
+    },
+    secure: process.env.NODE_ENV === 'production', // Enable SSL verification in production
+  };
+
+  console.log(proxyOptions);
+  app.use(route, createProxyMiddleware(proxyOptions));
+});
 
 app.get<{}, MessageResponse>('/', (_req: Request, res: Response) => {
   res.json({
-    message: 'API location: api/v1',
+    message: 'Hello!',
   });
 });
 
-app.use('/api/v1', api);
 
 app.use(notFound);
 app.use(errorHandler);
